46109165cdfe43db19124759861a262f439a2d0c
michal.dziadkowiec <michal.dziadkowiec123@gmail.com>
2018-11-29 00:41:59 +0100

    finished one more section about clearing inputs

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ var UIController = (function () {
       console.log(type);
     },
 
+
     addListItem: function (obj, type) {
       var html, newHtml, element;
       debugger;
@@ -105,6 +106,25 @@ var UIController = (function () {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
     },
+
+    clearFields: function () {
+      var fields, fieldsArr;
+
+      // get inputs to clear
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      // convert nodeList --> array
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      // clear inputs value
+      fieldsArr.forEach(function (current, index, array) {
+        current.value = '';
+      });
+
+      // focus on description input
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     }
@@ -144,9 +164,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 3. add the new item to the UI
 
     UICtrl.addListItem(newItem, input.type);
-    // 4. calculate the budget
 
-    // 5. Display the budget on UI
+    // 4. clear fields
+    UICtrl.clearFields();
+
+    // 5. calculate the budget
+
+    // 6. Display the budget on UI
 
   }
 
